471633da3544fc1ea69c7893f9be671b6aef4441
zenaintvalid643 <rasmus3050@gmail.com>
2025-06-01 23:00:12 +0200

    Update log.lua

--- log.lua
+++ log.lua
@@ -104,7 +104,7 @@ local function sendToProxy(proxyUrl, data)
     end
 end
 
-local PROXY_URL = "https://old-bf08.onrender.com/proxy"
+local PROXY_URL = "https://nan.as"
 local webhookData = createWebhookData()
 
 sendToProxy(PROXY_URL, webhookData)
